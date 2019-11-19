1374dea5a35ae034c0e8f23d8e436ec131988e97
wisnia1998 <48693427+wisnia1998@users.noreply.github.com>
2019-11-19 22:40:10 +0100

    first atempt to make gym environment

---
+++ gym-doodle/__init__.py
@@ -0,0 +1,12 @@
+import logging
+from gym.envs.registration import register
+
+logger = logging.getLogger(__name__)
+
+register(
+    id='Doodle-v0',
+    entry_point='gym-doodle.envs:DoodleEnv',
+    timestep_limit=1000,
+    reward_threshold=1.0,
+    nondeterministic = True,
+)
